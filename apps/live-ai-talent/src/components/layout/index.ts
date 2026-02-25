bf5d5f30dbd8fc0def52b23a3ff958a0e59d676d
jcrupi <john.crupi@kickanalytics.com>
2026-02-25 11:47:43 -0500

    chore: retire live-ai-talent (renamed to .IGNORE, gitignored)

--- apps/live-ai-talent/src/components/layout/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Logo } from './Logo'
-export { default as Navbar } from './Navbar'
-export { default as Sidebar } from './Sidebar'
-export { default as Footer } from './Footer'
-export { default as DashboardLayout } from './DashboardLayout'
--- apps/live-ai-talent/src/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Button } from './Button'
-export { default as Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './Card'
-export { default as Input } from './Input'
-export { default as Badge } from './Badge'
-export { default as Progress } from './Progress'
-export { default as Avatar } from './Avatar'
-export { default as Modal } from './Modal'
-export { Tabs, TabsList, TabsTrigger, TabsContent } from './Tabs'
